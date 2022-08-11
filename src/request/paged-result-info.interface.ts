c36afcb471cb6e0799cd1109d27e527d1fc2f120
johanneskuhs <johannes.kuhs@reactorone.com>
2022-08-11 16:30:32 -0400

    removed obsolete interface

--- src/request/paged-result-info.interface.ts
+++ src/request/paged-result-info.interface.ts
@@ -1,8 +1,3 @@
-export interface PagedResult<T=any> {
-    results: T[];
-    page: PagedResultInfo;
-}
-
 export interface PagedResultInfo {
     currentPage: number; // 1-based
     totalPages: number;
