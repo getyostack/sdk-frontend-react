be78d7aaf2a6ecc488110f773d9513a3b719effa
johanneskuhs <johannes.kuhs@reactorone.com>
2022-08-23 15:15:51 -0400

    added data request handler options

--- src/app/app-setup-context.interface.ts
+++ src/app/app-setup-context.interface.ts
@@ -38,6 +38,12 @@ export interface AppSetupContext {
 
     registerDataRequestHandler(
         dataProviderId: string,
-        handlerFn: DataRequestHandlerFn
+        handlerFn: DataRequestHandlerFn,
+        options?: {
+            cache?: {
+                disable?: boolean;
+                defaultTtlMs?: number;
+            }
+        }
     ): boolean;
 }
